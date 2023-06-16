ff03e4fc47d579217aca50354caf6bfbd8883dd0
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2023-06-16 15:07:56 -0300

    Navbar e footer

--- costs/src/components/layout/Footer.js
+++ costs/src/components/layout/Footer.js
@@ -1,13 +1,17 @@
 import{FaInstagram, FaFacebook, FaLinkedin, FaGithub} from 'react-icons/fa'
+import styles from './Footer.module.css'
 
 
 function Footer() {
-    return <footer >
-                <ul>
-                    <li><FaInstagram/></li>
-                    <li><FaLinkedin /></li>
-                    <li><FaGithub /></li>
+    return <footer className={styles.footer} >
+                <ul className={styles.social_list}>
+                    <li ><FaInstagram/></li>
+                    <li ><FaLinkedin /></li>
+                    <li ><FaGithub/></li>
                 </ul>
+                <p className={styles.copy_right}>
+                    <span>Raphael Oliveira</span> &copy; 2023
+                </p>
          </footer>
 }
 
