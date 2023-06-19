2bb61fa85664baa4fb338c6ade2fe3897e6af7c3
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2023-06-18 23:52:45 -0300

    Inserindo dados no banco

--- costs/src/pages/NewProject.js
+++ costs/src/pages/NewProject.js
@@ -1,15 +1,37 @@
 import ProjectForm from '../components/Project/ProjectForm'
+
 import styles from './Newproject.module.css'
 
-function NewProject() {
+import {useNavigate} from 'react-router-dom'
+
+export default function NewProject() {
+
+    const history = useNavigate()
+
+    function createPost(project){
+
+        project.cost=0
+        project.services = []
+
+        fetch("http:/localhost:5000/projects", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',},
+            body: JSON.stringify(project),
+        })
+        .then((resp)=> resp.json())
+        .then((data) => {
+            console.log(data)
+        })
+        .catch((err) => console.log(err))
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviçõs</p>
-            <ProjectForm btnText="Criar projeto" />
+            <ProjectForm handleSubmit={createPost}  btnText="Criar projeto" />
         </div>
 
     )
 }
-
-export default NewProject
